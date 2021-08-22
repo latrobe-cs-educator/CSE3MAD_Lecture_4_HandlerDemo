4b73e1fd38ac7164aa3c0e3904afff7d1e466723
Marita Fitzgerald <maritafitzgerald@gmail.com>
2021-08-22 18:59:39 +1000

    Update to 2021

--- app/src/main/java/com/example/handlerdemo/MainActivity.java
+++ app/src/main/java/com/example/handlerdemo/MainActivity.java
@@ -12,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //debug variable
+    String TAG = "MActivity";
+
     Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
         public void handleMessage(Message msg) {
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             TextView myTextView =
                     (TextView) findViewById(R.id.myTextView);
             myTextView.setText(string);
+            Log.d(TAG, "In handleMessage, string from bundle is " + string);
         }
     };
 
@@ -32,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void blockUiBtnClick(View view) {
         //long task to run in main UI thread
+        Log.d(TAG, "Block UI button is clicked, beginning long task");
         long endTime = System.currentTimeMillis() + 20 * 2000;
 
         while (System.currentTimeMillis() < endTime) {
@@ -45,12 +50,14 @@ public class MainActivity extends AppCompatActivity {
         }
         TextView myTextView = (TextView) findViewById(R.id.myTextView);
         myTextView.setText(R.string.btn_msg);
+        Log.d(TAG, "Block UI button is clicked, end long task");
     }
 
     public void threadBtnClick(View view) {
         Runnable runnable = new Runnable() {
             public void run() {
                 //long task to run in new thread
+                Log.d(TAG, "In threadBtnClick, in run() starting long task");
                 long endTime = System.currentTimeMillis() + 20 * 2000;
 
                 while (System.currentTimeMillis() < endTime) {
@@ -67,11 +74,14 @@ public class MainActivity extends AppCompatActivity {
                 Bundle bundle = new Bundle();
                 bundle.putString("myKey", "Hi from another Thread");
                 msg.setData(bundle);
+                Log.d(TAG, "Sending message to main UI thread after long task");
                 handler.sendMessage(msg);
             }
         };
 
+        //create new thread to run long task
         Thread mythread = new Thread(runnable);
+        Log.d(TAG, "Starting new thread");
         mythread.start();
 
     }
